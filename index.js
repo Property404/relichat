9604c6ac100c47c5b9bec2e21276cccd727b081f
Property404 <daganboy@gmail.com>
2016-12-03 15:51:19 +0000

    Added some basic logic

--- index.js
+++ index.js
@@ -10,6 +10,9 @@ app.get("/", function(req, res){
 
 io.on("connection", function(socket){
 	console.log("a user connected");
+	socket.on("disconnect", function(){
+		console.log("User disconnected");
+	});
 });
 
 http.listen(3000, function(){
