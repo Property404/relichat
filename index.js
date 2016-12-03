b42e740485b6a9589f525dbb159b02b2d49ec157
Property404 <daganboy@gmail.com>
2016-12-03 15:28:32 +0000

    Some other commit

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 var express = require("express");
 var app = express();
 var http = require("http").Server(app);
-
+var io = require("socket.io")(http);
 
 app.use("/static", express.static("./static"));
 app.get("/", function(req, res){
 	res.sendFile(__dirname + "/index.html");
+});
+
+io.on("connection", function(socket){
+	console.log("a user connected");
 });
 
 http.listen(3000, function(){
